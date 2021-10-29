dfe5e3efce40f5d899ad434fbba8d433a6f1470c
hallerk <kevin.haller@sincrodigital.com>
2021-10-29 14:38:10 -0700

    Created new component to display all the expenses and passed the full object to it

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
     const expenses = [
@@ -31,11 +31,7 @@ function App() {
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem
-                title={expenses[0].title}
-                amount={expenses[0].amount}
-                date={expenses[0].date}
-            />
+            <Expenses data={expenses} />
         </div>
     );
 }
