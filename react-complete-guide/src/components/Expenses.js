fad23ede11fe20d7aa06d70f78d4d1fd7a6404f6
hallerk <kevin.haller@sincrodigital.com>
2021-10-29 14:58:08 -0700

    Simplified the expenses component with map()

--- react-complete-guide/src/components/Expenses.js
+++ react-complete-guide/src/components/Expenses.js
@@ -3,26 +3,13 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
     return (
         <div className="expenses">
-            <ExpenseItem
-                title={props.data[0].title}
-                amount={props.data[0].amount}
-                date={props.data[0].date}
-            />
-            <ExpenseItem
-                title={props.data[1].title}
-                amount={props.data[1].amount}
-                date={props.data[1].date}
-            />
-            <ExpenseItem
-                title={props.data[2].title}
-                amount={props.data[2].amount}
-                date={props.data[2].date}
-            />
-            <ExpenseItem
-                title={props.data[3].title}
-                amount={props.data[3].amount}
-                date={props.data[3].date}
-            />
+            {props.data.map((expenses) => (
+                <ExpenseItem
+                    title={expenses.title}
+                    amount={expenses.amount}
+                    date={expenses.date}
+                />
+            ))}
         </div>
     );
 }
