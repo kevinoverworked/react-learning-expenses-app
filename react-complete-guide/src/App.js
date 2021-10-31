bf8212417b71e02a510980d71f7afdc8353a949d
hallerk <kevin.haller@sincrodigital.com>
2021-10-31 13:37:17 -0700

    Assignment to create an ExpensesFilter component and have it listen to user input and lift the state up to Expenses

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+
 function App() {
     const expenses = [
         {
--- react-complete-guide/src/components/Expenses/Expenses.js
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
+    const [enteredYear, setFilteredYear] = useState("2021");
+
+    const saveExpenseYearHandler = (enteredYear) => {
+        console.log("in Expenses.js");
+        console.log(enteredYear);
+        setFilteredYear(enteredYear);
+    };
+
     return (
-        <Card className="expenses">
-            {props.data.map((expenses) => (
-                <ExpenseItem
-                    title={expenses.title}
-                    amount={expenses.amount}
-                    date={expenses.date}
-                />
-            ))}
-        </Card>
+        <div>
+            <Card className="expenses">
+            <ExpensesFilter selected={enteredYear} onSaveYear={saveExpenseYearHandler} />
+                {props.data.map((expenses) => (
+                    <ExpenseItem
+                        title={expenses.title}
+                        amount={expenses.amount}
+                        date={expenses.date}
+                    />
+                ))}
+            </Card>
+        </div>
+        
     );
 }
 
