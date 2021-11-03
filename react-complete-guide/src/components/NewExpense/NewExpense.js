0cf8af974e6d97f2c243accc32a35e8be751b454
hallerk <kevin.haller@sincrodigital.com>
2021-11-02 21:37:16 -0700

    Rendering the add expense form conditionally

--- react-complete-guide/src/components/NewExpense/NewExpense.js
+++ react-complete-guide/src/components/NewExpense/NewExpense.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+    const [newExpense, setNewExpense] = useState(true);
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        setNewExpense(true);
+    };
+
+    const newExpenseClickHandler = () => {
+        setNewExpense(false);
+    };
+
+    const cancelExpenseClickHandler = () => {
+        setNewExpense(true);
     };
 
     return (
         <div className="new-expense">
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {newExpense && <button type="button" onClick={newExpenseClickHandler}> Add New Expense</button>}
+            {!newExpense && <ExpenseForm onCancel={cancelExpenseClickHandler} onSaveExpenseData={saveExpenseDataHandler} />}
         </div>
     )
 };
